b0899cb762952f02b44c3a68a5b4a88ebc70df1e
Miftakhul Hafidzun Nafi <miftakhul.hafidz11@gmail.com>
2021-09-12 21:11:46 +0700

    perbaiki style table

--- suratmasuk.php
+++ suratmasuk.php
@@ -10,6 +10,14 @@ $result = mysqli_query($mysqli, "SELECT * FROM suratmasuk ORDER BY id DESC");
 
 <head>
     <link rel='stylesheet' type='text/css' media='screen' href='styletable.css'>
+    <style>
+        .container td,
+        .container th {
+            padding-bottom: 1%;
+            padding-top: 1%;
+            padding-left: 1%;
+        }
+    </style>
 </head>
 
 <body>
@@ -19,9 +27,12 @@ $result = mysqli_query($mysqli, "SELECT * FROM suratmasuk ORDER BY id DESC");
         <div class="button"><a href="halaman_admin_suratmasuk_add.php">+ Tambahkan Surat Masuk</a></div>
     </center>
     <br>
-    <table class="container">
+    <table style="width: 95% !important;" class="container">
         <thead>
             <tr>
+                <th>
+                    <h1>No</h1>
+                </th>
                 <th>
                     <h1>Pengirim</h1>
                 </th>
@@ -37,13 +48,16 @@ $result = mysqli_query($mysqli, "SELECT * FROM suratmasuk ORDER BY id DESC");
             </tr>
         </thead>
         <?php
+        $number = 1;
         while ($user_data = mysqli_fetch_array($result)) {
             echo "<tbody>";
             echo "<tr>";
+            echo "<td> $number </td>";
             echo "<td>" . $user_data['pengirim'] . "</td>";
             echo "<td>" . $user_data['nomor_surat'] . "</td>";
             echo "<td>" . $user_data['perihal'] . "</td>";
-            echo "<td><a href='halaman_admin_suratmasuk_edit.php?id=$user_data[id]'>Edit</a> | <a href='suratmasuk_delete.php?id=$user_data[id]'>Delete</a></td></tr><tbody/>";
+            echo "<td><a href='halaman_admin_suratmasuk_edit.php?id=$user_data[id]'><i class='bx bxs-pencil'></i></a> | <a href='suratmasuk_delete.php?id=$user_data[id]'><i class='bx bxs-trash-alt'></i></a></td></tr><tbody/>";
+            ++$number;
         }
         ?>
     </table>
