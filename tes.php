7424254bc85be7c51cc1319d030d9586408d5dad
Miftakhul Hafidzun Nafi <miftakhul.hafidz11@gmail.com>
2021-09-13 10:17:09 +0700

    add assets

--- tes.php
+++ tes.php
@@ -1,181 +1,82 @@
+<?php
+// Create database connection using config file
+include_once("koneksi.php");
+
+// Fetch all users data from database
+$result = mysqli_query($mysqli, "SELECT * FROM suratmasuk ORDER BY id DESC");
+?>
+
 <html>
 
 <head>
-    <style>
-        @import url(https://fonts.googleapis.com/css?family=Open+Sans:300,400,700);
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        * {
-            box-sizing: border-box;
-            font-family: "Open Sans", sans-serif;
-        }
+    <title>Arsip Surat Kota Samarinda </title>
 
-        h1 {
-            font-size: 3em;
-            font-weight: 300;
-            line-height: 1em;
-            text-align: center;
-            color: #005075;
-        }
-
-        .button {
-            display: inline-block;
-            padding: 5px 15px;
-            font-size: 1.5vw;
-            cursor: pointer;
-            text-align: center;
-            outline: none;
-            color: white;
-            background-color: #4caf50;
-            border: none;
-            border-radius: 15px;
-            box-shadow: 0 3px #999;
-        }
-
-        .button a {
-            text-decoration: none;
-            color: white;
-        }
-
-        .button:hover {
-            background-color: #3e8e41;
-        }
-
-        .button:active {
-            background-color: #3e8e41;
-            box-shadow: 0 3px #666;
-            transform: translateY(2px);
-        }
-
-        input[type=text],
-        select,
-        textarea {
-            width: 100%;
-            padding: 12px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-            resize: vertical;
-        }
-
-        label {
-            padding: 12px 12px 12px 0;
-            display: inline-block;
-        }
-
-        input[type=submit] {
-            background-color: #4CAF50;
-            color: white;
-            padding: 12px 20px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-            float: right;
-        }
-
-        input[type=submit]:hover {
-            background-color: #45a049;
-        }
-
-        .container {
-            border-radius: 5px;
-            background-color: #1f2739;
-            color: white;
-            padding: 20px;
-            width: 70%;
-            margin-left: auto;
-            margin-right: auto;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-        }
-
-        .col-25 {
-            float: left;
-            width: 25%;
-            margin-top: 6px;
-        }
-
-        .col-75 {
-            float: left;
-            width: 75%;
-            margin-top: 6px;
-        }
-
-        /* Clear floats after the columns */
-        .row:after {
-            content: "";
-            display: table;
-            clear: both;
-        }
-
-        /* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
-        @media screen and (max-width: 600px) {
-
-            .col-25,
-            .col-75,
-            input[type=submit] {
-                width: 100%;
-                margin-top: 0;
-            }
-        }
-    </style>
+    <!-- Bootstrap -->
+    <link href="../assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../assets/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="../assets/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <!-- Datatables -->
+    <link href="../assets/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="../img/icon.ico">
+    <!-- Custom Theme Style -->
+    <link href="../assets/build/css/custom.min.css" rel="stylesheet">
 </head>
 
 <body>
-    <h1>Tambahkan Surat Masuk</h1>
+    <h1>Arsip Surat Masuk</h1>
     <br>
     <center>
-        <div class="button"><i class="bx bx-home"></i> <a href="halaman_admin_suratmasuk.php">Home</a></div>
+        <div class="button"><a href="halaman_admin_suratmasuk_add.php">+ Tambahkan Surat Masuk</a></div>
     </center>
     <br>
-
-    <div class="container">
-        <form action="halaman_admin_suratmasuk_add.php" method="post" name="form1">
-            <div class="row">
-                <div class="col-25">
-                    <label for="pengirim">Pengirim</label>
-                </div>
-                <div class="col-75">
-                    <input type="text" name="pengirim">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-25">
-                    <label for="nomor_surat">Nomor Surat</label>
-                </div>
-                <div class="col-75">
-                    <input type="text" name="nomor_surat">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-25">
-                    <label for="perihal">Perihal</label>
-                </div>
-                <div class="col-75">
-                    <input type="text" name="perihal">
-                </div>
-            </div>
-            <br>
-            <div class="row">
-                <input type="submit" name="Submit" value="Add">
-            </div>
-        </form>
-    </div>
-
-    <?php
-    // Check If form submitted, insert form data into users table.
-    if (isset($_POST['Submit'])) {
-        $pengirim = $_POST['pengirim'];
-        $nomor_surat = $_POST['nomor_surat'];
-        $perihal = $_POST['perihal'];
-
-        // include database connection file
-        include_once("koneksi.php");
-
-        // Insert user data into table
-        $result = mysqli_query($mysqli, "INSERT INTO suratmasuk(pengirim,nomor_surat,perihal) VALUES('$pengirim','$nomor_surat','$perihal')");
-
-        // Show message when user added
-        echo "Data Berhasil Ditambahkan. <a href='halaman_admin_suratmasuk.php'>Lihat Semua Data</a>";
-    }
-    ?>
+    <table style="width: 95% !important;" class="container">
+        <thead>
+            <tr>
+                <th>
+                    <h1>No</h1>
+                </th>
+                <th>
+                    <h1>Pengirim</h1>
+                </th>
+                <th>
+                    <h1>Nomor Surat</h1>
+                </th>
+                <th>
+                    <h1>Perihal</h1>
+                </th>
+                <th>
+                    <h1>Update</h1>
+                </th>
+            </tr>
+        </thead>
+        <?php
+        $number = 1;
+        while ($user_data = mysqli_fetch_array($result)) {
+            echo "<tbody>";
+            echo "<tr>";
+            echo "<td> $number </td>";
+            echo "<td>" . $user_data['pengirim'] . "</td>";
+            echo "<td>" . $user_data['nomor_surat'] . "</td>";
+            echo "<td>" . $user_data['perihal'] . "</td>";
+            echo "<td><a href='halaman_admin_suratmasuk_edit.php?id=$user_data[id]'><i class='bx bxs-pencil'></i></a> | <a href='../suratmasuk_delete.php?id=$user_data[id]'><i class='bx bxs-trash-alt'></i></a></td></tr><tbody/>";
+            ++$number;
+        }
+        ?>
+    </table>
 </body>
 
 </html>
