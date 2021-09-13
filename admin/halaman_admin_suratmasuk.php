84c6d6a08b5a0d6c11cd91408f71d89c05dd9174
Miftakhul Hafidzun Nafi <miftakhul.hafidz11@gmail.com>
2021-09-13 09:39:56 +0700

    fix logout

--- admin/halaman_admin_suratmasuk.php
+++ admin/halaman_admin_suratmasuk.php
@@ -46,7 +46,7 @@
                 </a>
             </li>
             <li class="log_out">
-                <a href="logout.php">
+                <a href="../logout.php">
                     <i class='bx bx-log-out'></i>
                     <span class="links_name">Log out</span>
                 </a>
